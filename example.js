567ca6c6cc2427ec33ff331ce49ebee699ffe8fb
IdleKaiOs8 <123788739+IdleKaiOs8@users.noreply.github.com>
2023-01-28 15:47:54 -0300

    okkkkk

---
+++ example.js
@@ -0,0 +1,10 @@
+/** 
+ * # Parameters
+ * `Country|Region` - Connect your region on this translator
+*/
+var translator = [
+    "country|region",
+    [
+        ["af_af", "Afrikaans", "Afrikaans"]
+    ]
+]
